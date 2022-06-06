23bce83032f40521aa35fcc3e18a2a40694b9dbd
Tiago Rodrigues <tirodev@hotmail.com>
2022-06-06 00:32:07 -0300

    Add FluentValidation Extensions

---
+++ src/TiRoDev.DotNet.Scaffolds.All/Application/Validation/ValidationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TiRoDev.DotNet.Scaffolds.All.Application.Validation;
+
+public static class ValidationExtensions
+{
+    public static ValidationProblemDetails ToProblemDetails(this ValidationException validationException)
+    {
+        var problemDetails = new ValidationProblemDetails
+        {
+            Title = "One or more validation errors occurred.",
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+            Status = (int)HttpStatusCode.BadRequest
+        };
+        
+        foreach (var validationFailure in validationException.Errors)
+        {
+            if (problemDetails.Errors.ContainsKey(validationFailure.PropertyName))
+            {
+                problemDetails.Errors[validationFailure.PropertyName] = 
+                    problemDetails.Errors[validationFailure.PropertyName].Concat(new[] { validationFailure.ErrorMessage }).ToArray();
+                continue;
+            }
+            
+            problemDetails.Errors.Add(new KeyValuePair<string, string[]>(
+                validationFailure.PropertyName,
+                new []{ validationFailure.ErrorMessage }));
+        }
+        
+        return problemDetails;
+    }
+}
